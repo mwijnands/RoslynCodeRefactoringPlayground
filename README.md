66f2b329921c04278a8016040f0fbae17d88df21
Robert Haken <haken@havit.cz>
2015-08-13 21:12:19 +0200

    Code Refactoring (VSIX) project template upgrade.
    Code migrated to new VS2015 "Code Refactoring (VSIX)" project template
    (C#).

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Code related to [https://trydis.github.io/2015/01/03/roslyn-code-refactoring/](https://trydis.github.io/2015/01/03/roslyn-code-refactoring/).
+Upgraded by Robert Haken
--- src/IntroduceAndInitializeLocalField/IntroduceAndInitializeLocalField/Properties/AssemblyInfo.cs
+++ src/IntroduceAndInitializeLocalField/IntroduceAndInitializeLocalField/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CodeRefactoring1")]
+[assembly: AssemblyTitle("IntroduceAndInitializeLocalField")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CodeRefactoring1")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("IntroduceAndInitializeLocalField")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
